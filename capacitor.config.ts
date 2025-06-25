ada63b374f2b49819eb0f524077d6b53f7f47e94
John Corser <johnpc@umich.edu>
2025-06-24 23:41:34 -0400

    update android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,24 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.johncorser.local',
-  appName: 'jpc-local',
-  webDir: 'dist'
+  appId: "com.johncorser.local",
+  appName: "local.jpc",
+  webDir: "dist",
+  server: {
+    hostname: "local.jpc.io",
+    androidScheme: "https",
+  },
+  ios: {
+    contentInset: "always",
+    backgroundColor: "#DB7093",
+  },
+  plugins: {
+    LocalNotifications: {
+      smallIcon: "icon-48",
+      iconColor: "#488AFF",
+      // sound: "beep.wav",
+    },
+  },
 };
 
 export default config;
